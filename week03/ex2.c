73955bb0d1dea6b7c7f3177c359030265fcdb5e5
Evsey <evseyru1@mail.ru>
2022-09-18 18:26:00 +0300

    added exercise2 of week03

---
+++ week03/ex2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct{
+	float x;
+	float y;
+} Point;
+
+float distance(Point A, Point B){
+	float result;
+	float diff_x = B.x - A.x;
+	float diff_y = B.y - A.y;
+	result = sqrtf(diff_x * diff_x + diff_y * diff_y);
+	return result;
+}
+
+float area(Point A, Point B, Point C){
+	float result;
+	result = fabsf(A.x * B.y - B.x * A.y + B.x * C.y - C.x * B.y + C.x * A.y - A.x * C.y) / 2;
+	return result;
+}
+
+int main(void){
+
+	//<WRITE YOUR CODE HERE>
+	Point A = {2.5f, 6};
+	Point B = {1, 2.2f};
+	Point C = {10, 6};
+
+	float f = distance(A, B);
+	printf("A -- B distance is %f\n", f);
+
+
+	float a = area(A, B, C);
+	printf("Area of triangle ABC is %f\n", a);
+
+	return EXIT_SUCCESS;
+}
